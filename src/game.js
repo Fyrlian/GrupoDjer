096a0f3113b1cac7e699566f43e1119da4b5f991
Jorgee-sk <73257086+Jorgee-sk@users.noreply.github.com>
2020-12-06 23:43:40 +0100

    Se configura el juego para que las escenas se adapten

--- src/game.js
+++ src/game.js
@@ -1,20 +1,32 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    width: window.innerWidth * window.devicePixelRatio,
+    height: window.innerHeight * window.devicePixelRatio,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
             debug: false
-        }
+
+        },
+        matter: {
+
+
+        },
     },
-    scene:[Scene0,Scene1,Scene2,Scene3]
+    scale: {
+        parent: 'yourgamediv',
+        mode: 'resize',
+        width: 800,
+        height: 600
+    },
+    scene:[Scene0,Scene1,Scene2,Scene3,Scene4,Scene5]
 };
 
 var player;
 var stars;
 var bombs;
+var enemigos;
 var platforms;
 var cursors;
 var score = 0;
@@ -25,6 +37,7 @@ var bullets;
 var speed;
 var lastFired = 0;
 var estado;
+var encimaDePlat =0;
 
 
 var game = new Phaser.Game(config);
