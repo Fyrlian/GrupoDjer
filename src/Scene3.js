83fece98bcdca0bd7950a9affc31d68a799ab961
Jorgee-sk <73257086+Jorgee-sk@users.noreply.github.com>
2020-12-06 23:40:48 +0100

    Añadido código relacionado con enemigos

--- src/Scene3.js
+++ src/Scene3.js
@@ -10,199 +10,196 @@ class Scene3 extends Phaser.Scene {
     
     preload(){
        
-/*
 
-        this.load.image('sueloMapa','assets/sueloMapa.png')
-        this.load.image('suelo','assets/suelo.png')
-        this.load.image('sueloMedio','assets/sueloMedio.png')
-        this.load.image('sueloMedio2','assets/sueloMedio2.png')
-        this.load.image('fondo','assets/Nivel 1.png')
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.image('bullet', 'assets/star.png');
-        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-        this.load.spritesheet('mainch', 'assets/mainchAK-47.png', { frameWidth: 100, frameHeight: 100 });
-        this.load.spritesheet('mainchizq', 'assets/mainchAK-47Izq.png', { frameWidth: 100, frameHeight: 100 });
-*/
     }
     create(){//  A simple background for our game
-    this.bg=this.add.image(960, 540, 'fondo');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.physics.add.staticGroup();
-
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(960, 917, 'sueloMapa');
-    platforms.create(925, 910, 'suelo');
-    platforms.create(840, 650, 'sueloMedio');
-    platforms.create(1462, 650, 'sueloMedio2');
-
-    //  Now let's create some ledges
-    platforms.create(160, 890, 'escalera1');
-    platforms.create(250, 845, 'escaleradefault');
-    platforms.create(305, 800, 'escaleradefault');
-    platforms.create(355, 755, 'escaleradefault');
-    platforms.create(410, 710, 'escaleradefault');
-    platforms.create(470, 665, 'escaleradefault');
-
-    
-    platforms.create(1062, 890, 'escaleradefault');
-    platforms.create(1125, 843, 'escaleradefault');
-    platforms.create(1185, 798, 'escaleradefault');
-    platforms.create(1237, 753, 'escaleradefault');
-    platforms.create(1297, 708, 'escaleradefault');
-
-    // The player and its settings
-    player = this.physics.add.sprite(160, 100, 'dude');
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    
-
-
-
-
-    var Bullet = new Phaser.Class({
-
-        Extends: Phaser.GameObjects.Image,
-
-        initialize:
-
-        function Bullet (scene)
-        {
-            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
-            this.speed = Phaser.Math.GetSpeed(400, 1);
-           
-        },
-
-        fire: function (x, y)
-        {
-
-            if(estado == 0)
-            this.setPosition(x-25, y);
-            
-
-            if(estado == 1)
-            this.setPosition(x+25, y);
-
-
-            this.setActive(true);
-            this.setVisible(true);
+        this.bg=this.add.image(config.width/2,config.height/2, 'fondo');
         
-        },
-        update: function (time, delta)
-        {
-                
-            if(estado == 0){
-              /*  this.setActive(false);  ///ESTO HAY QUE CAMBIARLO WACHO
-                this.setVisible(false);
-*/
-                
-            this.x -= this.speed * delta;  // Velocidad a la izquierda en X
-            
-            if (this.x < player.x-200)   //Rango de la bala para la izquierda
-            {
-                this.setActive(false);
-                this.setVisible(false); 
-            }
+        //Audio 
+        var audio1 = this.sound.add('audioScene1',{volume: 0.09,loop: true});
+        audio1.play();
 
+        //( The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = this.physics.add.staticGroup();
+        //  Here we create the ground.
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        platforms.create(config.width/2, config.height/1.114, 'sueloMapa');
+        platforms.create(config.width/2.07, config.height/1.12, 'suelo');
+        platforms.create(config.width/2.5, config.height/1.628, 'sueloMedio');
+        //platforms.create(config.width/2, config.height/1.55, 'sueloMedio2');
 
-            }
-            if(estado==1)
-            {
-
-            this.x += this.speed * delta;  // velocidad a la derecha en X
-            
-            if (this.x > player.x+200)  //rango de bala para la derecha
-            {
-                this.setActive(false);
-                this.setVisible(false); 
-            }
-
-         }
-
-
-        }
-           
-            
+        //  Now let's create some ledges
+        
+        //PRIMER ESCALON GOOORDO
+        platforms.create((config.width/2)/6,(config.height/2)/0.575, 'escalonGrande');
+        //ESCALONES NORMALES
+        platforms.create((config.width/2)/3.9, (config.height/2)/0.608, 'escalon');
+        platforms.create((config.width/2)/3.12,(config.height/2)/0.646, 'escalon');
+        platforms.create((config.width/2)/2.68,(config.height/2)/0.688, 'escalon');
+        platforms.create((config.width/2)/2.33,(config.height/2)/0.734, 'escalon');
         
 
+        
+        platforms.create(1062, 890, 'escalon');
+        platforms.create(1125, 843, 'escalon');
+        platforms.create(1185, 798, 'escalon');
+        platforms.create(1237, 753, 'escalon');
+        platforms.create(1297, 708, 'escalon');
+        
+        
+
+        // The player and its settings
+        player = this.physics.add.sprite(160, 100, 'dude');
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        
+        
+
+        this.physics.add.collider(
+            player,
+            platforms,
+            function (_player, _platforms)
+            {
+                if (_player.body.touching.down && _platforms.body.touching.up)
+                {
+                    game.encimaDePlat = 1;
+                }else{
+                    game.encimaDePlat= 0;
+                }
+                
+            });
+
+
+    
+
+        speed = Phaser.Math.GetSpeed(300, 1);
+        
+
+        //  Our player animations, turning, walking left and walking right.
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('mainchizq', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+            
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'mainch', frame: 1 } ],
+            frameRate: 5,
+            
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('mainch', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+            
 
         });
 
-
-        bullets = this.add.group({
-        classType: Bullet,
-        maxSize: 2,
-        runChildUpdate: true
-        
-    });
-
-    speed = Phaser.Math.GetSpeed(300, 1);
+        this.anims.create({
+            key: 'Enemigoleft',
+            frames: this.anims.generateFrameNumbers("zombieLeft"),
+            frameRate: 5,
+            repeat: -1
+            
+        });
     
-
-    //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('mainchizq', { start: 0, end: 3 }),
-        frameRate: 5,
-        repeat: -1
-        
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'mainch', frame: 1 } ],
-        frameRate: 5,
-        
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('mainch', { start: 0, end: 3 }),
-        frameRate: 5,
-        repeat: -1
-        
-
-    });
-
-    //  Input Events
-    cursors = this.input.keyboard.createCursorKeys();
+        this.anims.create({
+            key: 'Enemigoright',
+            frames: this.anims.generateFrameNumbers("zombieRight"),
+            frameRate: 5,
+            repeat: -1
     
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
+        });
 
-    stars.children.iterate(function (child) {
+        //  Input Events
+        cursors = this.input.keyboard.createCursorKeys();
+        
+        //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+        stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
 
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        stars.children.iterate(function (child) {
 
-    });
+            //  Give each star a slightly different bounce
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-    bombs = this.physics.add.group();
+        });
 
-    //  The score
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        bombs = this.physics.add.group();
 
-    //  Collide the player and the stars with the platforms
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(bombs, platforms);
+        //  The score
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(player, stars, this.collectStar, null, this);
-    this.physics.add.collider(player, bombs, this.hitBomb, null, this);
+        //  Collide the player and the stars with the platforms
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.collider(bombs, platforms);
+
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        this.physics.add.overlap(player, stars, this.collectStar, null, this);
+        this.physics.add.collider(player, bombs, this.hitBomb, null, this);
+
+        //GRUPO DE LOS ENEMIGOS
+        this.enemigos = this.add.group();
+
+        this.enemigos.add(new Enemigo(this, 200, 200));
+
+        this.physics.add.collider(this.enemigos, platforms);
+
+
+
+        //GRUPO DE LAS BALAS
+        this.balas = this.add.group();
+        for(var i = 0; i < 50; i++){//inicializamos 50 balas
+
+            this.balas.getChildren()[i] = new Bala(this);
+
+        }
+
+        this.input.keyboard.on("keydown_SPACE",() => {//pulsar el boton de disparo
+            
+            var i = 0;
+            while(this.balas.getChildren()[i].x != -1){//encontramos la bala que usaremos
+
+                i++;
+
+            }
+            if(i >= 50 - 1){
+                    i = 0;
+            }      
+        this.balas.getChildren()[i].x = player.x;
+        this.balas.getChildren()[i].y = player.y;
+
+        if(estado == 0){
+            this.balas.getChildren()[i].body.setVelocityX(-1000);
+        }
+        else{
+            this.balas.getChildren()[i].body.setVelocityX(1000);
+        }
+            
+        })
+
     }
+    
 
     update(time,delta){
+        //UPDATE ENEMIGOS
+        for(var i = 0; i < this.enemigos.getChildren().length; i++){
+            var enem = this.enemigos.getChildren()[i];
+            enem.update(player.x);
+          }
+
+
         if (gameOver)
         {
             return;
@@ -234,22 +231,21 @@ class Scene3 extends Phaser.Scene {
         {
             player.setVelocityY(-200);
         }
-    
-        if (cursors.space.isDown && time > lastFired)
-        {
-            var bullet = bullets.get();
-    
-            if (bullet)
-            {
-                bullet.fire(player.x, player.y);
-    
-                lastFired = time + 50;
-            }
-        }
+
     
     
     }
 
+    createStar(x, y, vx, vy)
+{
+    var star = stars.get();
+
+    if (!star) return;
+
+    star
+        .enableBody(true, x, y, true, true)
+        .setVelocity(vx, vy);
+}
 collectStar (player, star){
         star.disableBody(true, true);
     
