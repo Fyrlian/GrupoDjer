26d72ef7ae4bc9d0e5f1875c61891c8655565203
Jorgee-sk <73257086+Jorgee-sk@users.noreply.github.com>
2020-12-09 13:29:46 +0100

    Actualizado las variables y configuración del juego

--- src/game.js
+++ src/game.js
@@ -6,38 +6,48 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: false
+            debug: true
 
         },
         matter: {
-
-
+            gravity: { y: 300 },
+            debug: false
         },
     },
     scale: {
         parent: 'yourgamediv',
-        mode: 'resize',
-        width: 800,
-        height: 600
+        mode: 'fit',
+        width: 1920,
+        height: 1080
     },
-    scene:[Scene0,Scene1,Scene2,Scene3,Scene4,Scene5]
+    scene:[Scene0,Scene1,Scene2,Scene3,Scene4,Scene5,Scene6,Scene7,Scene8]
 };
 
 var player;
+var player2;
 var stars;
 var bombs;
 var enemigos;
 var platforms;
+var escaleras,escaleras1,escaleras2,tejas,tejas2;
 var cursors;
 var score = 0;
-var gameOver = false;
 var scoreText;
+var cuentaAtras;
 var fireButton;
 var bullets;
 var speed;
 var lastFired = 0;
 var estado;
+var estado2;
 var encimaDePlat =0;
+var colliderEliminado = 0;
+var colliderEscalerasEliminado = 0;
+var iteracion = 0;
+var estadoSonido = true;
+var gameOver = false;
+
+var disparoSound;
 
 
 var game = new Phaser.Game(config);
