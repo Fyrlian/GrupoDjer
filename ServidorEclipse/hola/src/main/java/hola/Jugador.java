a0f5905fda53f1bca7b28f7cb692f14979b369fc
Danielin <sowerdani@gmail.com>
2021-01-05 19:42:25 +0100

    Introducion del servidor
    Servidor eclipse con java EE

---
+++ ServidorEclipse/hola/src/main/java/hola/Jugador.java
@@ -0,0 +1,18 @@
+package hola;
+
+public class Jugador {
+
+	
+	public int posicionx = 0;
+	public int posiciony = 0;
+	
+	public Jugador(int posicionx_, int posiciony_ ) {
+		
+		posicionx = posicionx_;
+		posicionx = posiciony_;
+		
+	}
+	
+	public Jugador() {}
+	
+}
