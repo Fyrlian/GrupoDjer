db742f3cdde0e81a8819b6d17a2a5b389a252263
Jorgee-sk <juxxjorge@gmail.com>
2021-01-13 16:54:58 +0100

    Servidor mejorando

--- ServidorEclipse/hola/src/main/java/hola/Mensaje.java
+++ ServidorEclipse/hola/src/main/java/hola/Mensaje.java
@@ -4,5 +4,13 @@ public class Mensaje {
 	
 public String texto;
 public String usuario;
+
+public Mensaje() {}
+
+public Mensaje(String _usuario, String _texto) {
 	
+	usuario = _usuario;
+	texto = _texto;
+	
+}
 }
