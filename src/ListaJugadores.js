1d2cccb091a4be4bf0921dd1da8b0bdeb7e499bc
kalomano <73484962+kalomano@users.noreply.github.com>
2021-01-13 19:17:14 +0100

    Aviso de conexion y desconexion
    Mediante el ListaJugadores con el metodo Get que ya existia

--- src/ListaJugadores.js
+++ src/ListaJugadores.js
@@ -11,6 +11,8 @@ class ListaJugadores extends Phaser.Scene {
         
     }
     create(){
+
+        
         that3 = this;
         this.add.image(config.width/2,config.height/3,'chat');
         this.add.image(config.width/2,config.height/1.5,'chat');
@@ -39,30 +41,80 @@ $.ajax({
 
     url: "http://localhost:8080/conectado"
 }).then(function(data) {
+ 
 
-   
+ 
     //EL JUGADOR1 EXISTE
     if(data[0] != null){
+
+        estaConectadoPlayer1 = true;
+   
+        if(estaConectadoPlayer1 != estaConectadoPlayer1aux){
+    
+            alert("Se ha conectado Jugador 1: " + data[0]);
+    
+        }
+        estaConectadoPlayer1aux = true;
+
+
         that3.textPlayer1.setText("Player 1: "+ data[0].usuario);
         that3.equisPlayer1.setVisible(false);
+
 
         
     //EL JUGADOR1 NO EXISTE
     }else{
+
+        estaConectadoPlayer1 = false;
+   
+        if(estaConectadoPlayer1 != estaConectadoPlayer1aux){
+    
+            alert("Se ha desconectado Jugador 1: " + data[0]);
+    
+        }
+        estaConectadoPlayer1aux = false;
+
+
         that3.equisPlayer1.setVisible(true);
+
     }
 
     
     //EL JUGADOR2 EXISTE
     
     if(data[1] != null){
+
+        estaConectadoPlayer2 = true;
+   
+        if(estaConectadoPlayer2 != estaConectadoPlayer2aux){
+    
+            alert("Se ha conectado Jugador 2: " + data[1]);
+    
+        }
+        estaConectadoPlayer2aux = true;
+
+
         that3.textPlayer2.setText("Player 2: "+ data[1].usuario);
         that3.equisPlayer2.setVisible(false);
 
 
+
     //EL JUGADOR2 NO EXISTE
     }else{
+
+        estaConectadoPlayer2 = false;
+   
+        if(estaConectadoPlayer2 != estaConectadoPlayer2aux){
+    
+            alert("Se ha desconectado Jugador 2: " + data[1]);
+    
+        }
+        estaConectadoPlayer1aux = false;
+
+
         that3.equisPlayer2.setVisible(true);
+
+
     }
 
     
@@ -76,6 +128,6 @@ $.ajax({
 
 
 
-    }
-}
+    }//FIN UPDATE
+}//FIN ESCENA
 
